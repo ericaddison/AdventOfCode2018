91141ba7712a7e209a62c9858d14920a6049c50c
Eric Addison <ericaddison@gmail.com>
2018-12-04 09:19:00 -0800

    Added part 2 for day 1

--- src/1/compute_freq_delta.py
+++ src/1/compute_freq_delta.py
@@ -5,9 +5,48 @@
 # From a list of frequency deltas df_i, compute the final frequency f = sum(df_i)
 #
 
-frequency = 0
-with open('./input/delta_freq.dat') as in_file:
-  for line in in_file:
-    frequency = frequency + int(line)
 
-print("The final frequency is:", frequency)
+def read_input(file_path):
+### open file and read deltas
+  deltas = []
+  with open(file_path) as in_file:
+    for line in in_file:
+      deltas.append(int(line))
+    return deltas
+
+
+def delta_loop(frequency, deltas, frequency_history, look_for_first_duplicate=False):
+### loop over all deltas to find final frequency
+#   If first duplicate is found, print it out
+#   return final frequency
+
+  for delta in deltas:
+    frequency = frequency + delta
+    if look_for_first_duplicate and frequency in frequency_history:
+      look_for_first_duplicate = False
+      print("Found first duplicate frequency:", frequency)
+    frequency_history.add(frequency)
+  return frequency, look_for_first_duplicate
+
+
+def main():
+  # read the input
+  deltas = read_input('./input/delta_freq.dat')
+
+  # initialize running frequency value and history set
+  frequency = 0
+  frequency_history = set()
+  frequency_history.add(frequency)
+  look_for_first_duplicate = True
+
+  # do the first loop
+  frequency, look_for_first_duplicate = delta_loop(frequency, deltas, frequency_history, look_for_first_duplicate)
+  print("The final frequency after the first loop is:", frequency)
+
+  # continue to loop until first duplicate is found
+  while look_for_first_duplicate:
+    frequency, look_for_first_duplicate = delta_loop(frequency, deltas, frequency_history, look_for_first_duplicate)
+  
+
+if __name__== "__main__":
+  main()
